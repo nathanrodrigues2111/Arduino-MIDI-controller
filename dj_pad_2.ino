f795826a549b16ae4663bd29e1d95f594ac79dbb
nathanrodrigues2111 <nathanrodrigues2111@gmail.com>
2017-05-09 09:03:26 +0530

    Update dj_pad_2.ino

--- dj_pad_2.ino
+++ dj_pad_2.ino
@@ -1,9 +1,26 @@
+/* (c) Arduino MIDI Controller by Nathan Rodrigues
+ * Email : nathanrodrigues2111@gmail.com
+ * YouTube : https://www.youtube.com/channel/UCu7i2pUEoPFWwhd_kBC_zuA
+ * Website : ??????
+ *
+ * Edit it however you wish, As long as you know what you are doing.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ /
+ 
+
 // include MIDI library
 #include <MIDI.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
  
-// read digital pins number 2, 4, 7
+// read digital pins number 2, 3, 4, 5
 int pushButton2 = 2;
 int pushButton3 = 3;
 int pushButton4 = 4;
@@ -21,9 +38,8 @@ int note3 = 0;
 int note4 = 0;
 int note5 = 0;
 
-
 //Analog buttons
-int val = 0; //Our initial pot values. We need one for the first value and a second to test if there has been a change made. This needs to be done for all 3 pots.
+int val = 0; //Our initial pot values. We need one for the first value and a second to test if there has been a change made. This needs to be done for all 5 pots.
 int lastVal = 0;
 int val2 = 0;
 int lastVal2 = 0;
@@ -34,12 +50,9 @@ int lastVal4 = 0;
 int val5 = 0;
 int lastVal5 = 0;
 
-
-
-
 void setup() {
   MIDI.begin(4);
-  // 115200 hairless MIDI
+  // 38400 hairless MIDI
   Serial.begin(38400);
 
   pinMode(13,OUTPUT);
@@ -58,6 +71,7 @@ void loop() {
   int buttonState4 = digitalRead(pushButton4);
   int buttonState5 = digitalRead(pushButton5);
 
+//Digital Buttons
 
    // Button 2
   if (buttonState2 == LOW) {
@@ -113,12 +127,12 @@ void loop() {
 
 
 
-  //Analog shit
+  //Analog Potentionmeter
 
   val = analogRead(0)/8;   // Divide by 8 to get range of 0-127 for midi
    if (val != lastVal) // If the value does not = the last value the following command is made. This is because the pot has been turned. Otherwise the pot remains the same and no midi message is output.
    {
-   MIDImessage(176,1,val);}         // 176 = CC command (channel 1 control change), 1 = Which Control, val = value read from Potentionmeter 1 NOTE THIS SAYS VAL not VA1 (lowercase of course)
+   MIDImessage(176,1,val);}         / // 176 = CC command, 2 = Which Control, val = value read from Potentionmeter 1
    lastVal = val;
 
    val2 = analogRead(1)/8;   // Divide by 8 to get range of 0-127 for midi
@@ -136,13 +150,13 @@ void loop() {
     val4 = analogRead(3)/8;   // Divide by 8 to get range of 0-127 for midi
    if (val4 != lastVal4) 
    {
-   MIDImessage(176,4,val4);}         // 176 = CC command, 2 = Which Control, val = value read from Potentionmeter 2
+   MIDImessage(176,4,val4);}         // 176 = CC command, 4 = Which Control, val = value read from Potentionmeter 4
    lastVal4 = val4;
    
    val5 = analogRead(4)/8;   // Divide by 8 to get range of 0-127 for midi
    if (val5 != lastVal5) 
    {
-   MIDImessage(176,5,val5);}         // 176 = CC command, 3 = Which Control, val = value read from Potentionmeter 3
+   MIDImessage(176,5,val5);}         // 176 = CC command, 5 = Which Control, val = value read from Potentionmeter 5
    lastVal5 = val5;
 delay(10); //here we add a short delay to help prevent slight fluctuations, knocks on the pots etc. Adding this helped to prevent my pots from jumpin up or down a value when slightly touched or knocked.
 }
